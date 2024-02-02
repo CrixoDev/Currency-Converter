8d6087eeb94782c7799b9d82228972abee45202d
Crixus <nickoplanel@gmail.com>
2024-02-01 23:07:45 -0300

    minor changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://v6.exchangerate-api.com/v6',
-    apiKey: 'APIKEY'
+    apiKey: 'ec01edb9c3997c6c378992ec'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://v6.exchangerate-api.com/v6',
-    apiKey: 'APIKEY'
+    apiKey: 'ec01edb9c3997c6c378992ec'
 };
